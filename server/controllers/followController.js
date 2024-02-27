b65c88497871bfb124bbeb171cc88d650ddc3052
Leston <lestonair6574@gmail.com>
2024-02-27 20:21:07 +0530

    getting followers,following and unfollow apis done

--- server/controllers/followController.js
+++ server/controllers/followController.js
@@ -5,21 +5,21 @@ exports.sendFollowRequest = async (req, res, next) => {
     try {
         const requester_id = req.params.requester_id;
         const receiver_id = req.params.receiver_id;
-        
-       
+
+
         const sendFollowRequestQuery = 'INSERT INTO FollowRequest (requester_id, receiver_id) VALUES (?, ?)';
         await executeQuery(sendFollowRequestQuery, [requester_id, receiver_id]);
 
-        const getExistingPendingFollowRequestsQuery='SELECT pending_follow_requests FROM User WHERE user_id = ?'
-        let results=await executeQuery(getExistingPendingFollowRequestsQuery,[receiver_id])
+        const getExistingPendingFollowRequestsQuery = 'SELECT pending_follow_requests FROM User WHERE user_id = ?'
+        let results = await executeQuery(getExistingPendingFollowRequestsQuery, [receiver_id])
 
-        let existingPendingRequests=results[0].pending_follow_requests
+        let existingPendingRequests = results[0].pending_follow_requests
         let pendingRequests
 
-        if(existingPendingRequests==null){
-            pendingRequests=`${requester_id}`
-        }else{
-            pendingRequests=existingPendingRequests+`,${requester_id}`
+        if (existingPendingRequests == null) {
+            pendingRequests = `${requester_id}`
+        } else {
+            pendingRequests = existingPendingRequests + `,${requester_id}`
         }
 
         const updatePendingFollowRequestsQuery = 'UPDATE User SET pending_follow_requests = ? WHERE user_id = ?';
@@ -42,44 +42,44 @@ exports.acceptFollowRequest = async (req, res, next) => {
         if (result && result.length > 0 && result[0].status === 'accepted') {
             return res.status(400).json({ message: 'Follow request already accepted' });
         }
-    
+
         const acceptFollowRequestQuery = 'UPDATE FollowRequest SET status = "accepted" WHERE requester_id = ? AND receiver_id = ?';
         await executeQuery(acceptFollowRequestQuery, [requester_id, receiver_id]);
 
-        const getExistingPendingFollowRequestsQuery='SELECT pending_follow_requests,following,accepted_followers FROM User WHERE user_id = ?'
-        let results=await executeQuery(getExistingPendingFollowRequestsQuery,[receiver_id])
+        const getExistingPendingFollowRequestsQuery = 'SELECT pending_follow_requests,following,accepted_followers FROM User WHERE user_id = ?'
+        let results = await executeQuery(getExistingPendingFollowRequestsQuery, [receiver_id])
 
-        let existingPendingRequests=results[0].pending_follow_requests
+        let existingPendingRequests = results[0].pending_follow_requests
 
         let valueToRemove = `${requester_id}`;
 
         let array = existingPendingRequests.split(",");
-      
+
         let filteredArray = array.filter(item => item !== valueToRemove);
 
         let resultString = filteredArray.join(",");
-      
+
         const removePendingFollowRequestQuery = 'UPDATE User SET pending_follow_requests = ?  WHERE user_id = ?';
         await executeQuery(removePendingFollowRequestQuery, [resultString, receiver_id]);
 
         let acceptedFollowers
-        if(results[0].accepted_followers==null){
-            acceptedFollowers=`${requester_id}`
-        }else{
-            acceptedFollowers=results[0].accepted_followers+`,${requester_id}`
+        if (results[0].accepted_followers == null) {
+            acceptedFollowers = `${requester_id}`
+        } else {
+            acceptedFollowers = results[0].accepted_followers + `,${requester_id}`
         }
         const addFollowerQuery = 'UPDATE User SET accepted_followers = ? WHERE user_id = ?';
         await executeQuery(addFollowerQuery, [acceptedFollowers, receiver_id]);
 
-        let getFollowingQuery='SELECT following FROM User WHERE user_id= ?'
-        let ans=await executeQuery(getFollowingQuery,[requester_id])
-        if(ans[0].following==null){
-            ans=`${receiver_id}`
-        }else{
-            ans=ans[0].following+`,${receiver_id}`
+        let getFollowingQuery = 'SELECT following FROM User WHERE user_id= ?'
+        let ans = await executeQuery(getFollowingQuery, [requester_id])
+        if (ans[0].following == null) {
+            ans = `${receiver_id}`
+        } else {
+            ans = ans[0].following + `,${receiver_id}`
         }
-        let updateFollowingQuery='UPDATE User SET following= ? WHERE user_id= ?'
-        await executeQuery(updateFollowingQuery,[ans,requester_id])
+        let updateFollowingQuery = 'UPDATE User SET following= ? WHERE user_id= ?'
+        await executeQuery(updateFollowingQuery, [ans, requester_id])
 
 
         const incrementFollowingQuery = 'UPDATE User SET no_of_following = no_of_following + 1 WHERE user_id = ?';
@@ -99,23 +99,23 @@ exports.rejectFollowRequest = async (req, res, next) => {
         const requester_id = req.params.requester_id;
         const receiver_id = req.params.receiver_id;
 
-    
+
         const rejectFollowRequestQuery = 'UPDATE FollowRequest SET status = "rejected" WHERE requester_id = ? AND receiver_id = ?';
         await executeQuery(rejectFollowRequestQuery, [requester_id, receiver_id]);
 
-        const getExistingPendingFollowRequestsQuery='SELECT pending_follow_requests FROM User WHERE user_id = ?'
-        let results=await executeQuery(getExistingPendingFollowRequestsQuery,[receiver_id])
+        const getExistingPendingFollowRequestsQuery = 'SELECT pending_follow_requests FROM User WHERE user_id = ?'
+        let results = await executeQuery(getExistingPendingFollowRequestsQuery, [receiver_id])
 
-        let existingPendingRequests=results[0].pending_follow_requests
+        let existingPendingRequests = results[0].pending_follow_requests
 
         let valueToRemove = `${requester_id}`;
 
         let array = existingPendingRequests.split(",");
-      
+
         let filteredArray = array.filter(item => item !== valueToRemove);
 
         let resultString = filteredArray.join(",");
-       
+
         const removePendingFollowRequestQuery = 'UPDATE User SET pending_follow_requests = ? WHERE user_id = ?';
         await executeQuery(removePendingFollowRequestQuery, [resultString, receiver_id]);
 
@@ -125,11 +125,62 @@ exports.rejectFollowRequest = async (req, res, next) => {
     }
 }
 
+exports.unFollow = async (req, res, next) => {
+    try {
+        const { initiator_id, respondent_id } = req.params;
+
+    
+        const getFollowingQuery = 'SELECT following FROM User WHERE user_id = ?';
+        let result = await executeQuery(getFollowingQuery, [initiator_id]);
+        let followingList = result[0].following;
+
+        if (followingList) {
+            const followingArray = followingList.split(",");
+            const filteredFollowing = followingArray.filter(item => item !== respondent_id);
+            const updatedFollowing = filteredFollowing.join(",");
+            const updateFollowingQuery = 'UPDATE User SET following = ? WHERE user_id = ?';
+            await executeQuery(updateFollowingQuery, [updatedFollowing, initiator_id]);
+        }
+
+       
+        const getAcceptedFollowersQuery = 'SELECT accepted_followers FROM User WHERE user_id = ?';
+        result = await executeQuery(getAcceptedFollowersQuery, [respondent_id]);
+        let acceptedFollowersList = result[0].accepted_followers;
+
+        if (acceptedFollowersList) {
+            const acceptedFollowersArray = acceptedFollowersList.split(",");
+            const filteredAcceptedFollowers = acceptedFollowersArray.filter(item => item !== initiator_id);
+            const updatedAcceptedFollowers = filteredAcceptedFollowers.join(",");
+            const updateAcceptedFollowersQuery = 'UPDATE User SET accepted_followers = ? WHERE user_id = ?';
+            await executeQuery(updateAcceptedFollowersQuery, [updatedAcceptedFollowers, respondent_id]);
+        }
+
+     
+        const decrementFollowingQuery = 'UPDATE User SET no_of_following = no_of_following - 1 WHERE user_id = ?';
+        await executeQuery(decrementFollowingQuery, [initiator_id]);
+
+        const decrementFollowersQuery = 'UPDATE User SET no_of_followers = no_of_followers - 1 WHERE user_id = ?';
+        await executeQuery(decrementFollowersQuery, [respondent_id]);
+
+        res.status(200).json({ message: 'Unfollowed successfully' });
+    } catch (error) {
+        next(error);
+    }
+}
+
 exports.getFollowers = async (req, res, next) => {
     try {
         const user_id = req.params.user_id;
-        const getFollowersQuery = 'SELECT accepted_followers FROM User WHERE user_id = ?';
-        const followers = await executeQuery(getFollowersQuery, [user_id]);
+
+        // const query='SELECT accepted_followers from User where user_id = ?'
+        // let result=await executeQuery(query,[user_id])
+        // console.log(result[0].accepted_followers)
+        // const getFollowersQuery='SELECT user_id, name, user_name FROM User WHERE FIND_IN_SET(?,?);'
+        // let followers=await executeQuery(getFollowersQuery,[user_id,result[0].accepted_followers])
+
+        const getFollowersQuery='SELECT u.user_id, u.name, u.user_name FROM User u WHERE FIND_IN_SET(u.user_id, (SELECT accepted_followers FROM User WHERE user_id = ?));'
+        let followers=await executeQuery(getFollowersQuery,[user_id])
+
         res.status(200).json(followers);
     } catch (error) {
         next(error);
@@ -139,8 +190,10 @@ exports.getFollowers = async (req, res, next) => {
 exports.getFollowing = async (req, res, next) => {
     try {
         const user_id = req.params.user_id;
-        const getFollowingQuery = 'SELECT pending_follow_requests FROM User WHERE user_id = ?';
-        const following = await executeQuery(getFollowingQuery, [user_id]);
+        
+        const getFollowingQuery='SELECT u.user_id, u.name, u.user_name FROM User u WHERE FIND_IN_SET(u.user_id, (SELECT following FROM User WHERE user_id = ?));'
+        let following=await executeQuery(getFollowingQuery,[user_id])
+
         res.status(200).json(following);
     } catch (error) {
         next(error);
--- server/routes/followRoutes.js
+++ server/routes/followRoutes.js
@@ -1,5 +1,5 @@
 const express=require("express")
-const { sendFollowRequest, acceptFollowRequest, rejectFollowRequest, getFollowers, getFollowing } = require("../controllers/followController")
+const { sendFollowRequest, acceptFollowRequest, rejectFollowRequest, getFollowers, getFollowing, unFollow } = require("../controllers/followController")
 
 
 const router=express.Router()
@@ -7,6 +7,7 @@ const router=express.Router()
 router.route("/sendfollowreq/:requester_id/:receiver_id").post(sendFollowRequest)
 router.route("/acceptfollowreq/:requester_id/:receiver_id").post(acceptFollowRequest)
 router.route("/rejectfollowreq/:requester_id/:receiver_id").post(rejectFollowRequest)
+router.route("/unfollow/:initiator_id/:respondent_id").post(unFollow)
 router.route("/getfollowers/:user_id").get(getFollowers)
 router.route("/getfollowing/:user_id").get(getFollowing)
 
